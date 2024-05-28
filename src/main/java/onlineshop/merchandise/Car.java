07c060f797461ef43e7c47b9cc2171999cd4f006
lukas <lukasp1209@gmail.com>
2024-05-28 15:01:44 +0200

    own csv import and changes

---
+++ src/main/java/onlineshop/merchandise/Car.java
@@ -0,0 +1,53 @@
+package onlineshop.merchandise;
+
+public class Car {
+    protected int id;
+    protected String brand;
+    protected String model;
+    protected int year;
+    protected String power;
+    protected double price;
+    protected String image;
+
+    public Car(int id, String brand, String model, int year, String power, double price, String image) {
+        this.id = id;
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.power = power;
+        this.price = price;
+        this.image = image;
+    }
+
+    public Car() {
+        
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getPower() {
+        return power;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getImage() {
+        return image;
+    }
+}
