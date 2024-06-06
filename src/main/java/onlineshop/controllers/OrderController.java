51d0c778403013e3735bf2f108c8a6412cd9aa74
lukas <lukasp1209@gmail.com>
2024-06-06 13:27:26 +0200

    fix bugs

--- src/main/java/onlineshop/controllers/OrderController.java
+++ src/main/java/onlineshop/controllers/OrderController.java
@@ -1,13 +1,16 @@
 package onlineshop.controllers;
 
 import onlineshop.*;
+import onlineshop.merchandise.Car;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.IOException;
 import java.util.List;
 
 @Controller
